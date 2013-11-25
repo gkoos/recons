8d2c673c1cb06fd5b9dbd2dde79f0628cb9ddffa
OpenShift guest <gabor.koos@gmail.com>
2013-11-25 15:09:03 -0500

    Seconds commit

--- README.md
+++ README.md
@@ -1,4 +1,29 @@
 recons
 ======
 
-Remote javascript console
+Recons is a remote javascript console inspired by [jsconsole](http://jsconsole.com)
+
+Features
+--------
+
+* Remote device debugging
+* Recieve console events
+* Send javascript commands to device
+
+Usage
+-----
+
+Recons is heavily inspired by [jsconsole](http://jsconsole.com). In fact, they share the same overall functionality, even most of the console commands are the same. The only significant difference is in the implementation: while jsconsole utilizes iframes, recons uses the socket.io library.
+
+Installation
+------------
+
+This requires node.js. Once you installed it, download this project (or clone it using git) and inside the new jsconsole directory run:
+
+npm install
+
+This will install the dependencies (in particular 1.8.x version of connect.js).
+
+Once installed, run (on port 80):
+
+node server.js
--- server.js
+++ server.js
@@ -1,92 +1,41 @@
-var http = require("http");
-var url = require("url");
-var qs = require("querystring");
-var fs = require('fs');
-var async = require('async');
+var http = require('http');
+var path = require('path');
+var fs   = require('fs');
+var url  = require("url");
+var sck  = require("socket.io");
 
-var html;
-var css;
+// mime types for static content
+var extensions = {
+".html": "text/html",
+".css": "text/css",
+".js": "application/javascript",
+".png": "image/png",
+".gif": "image/gif",
+".jpg": "image/jpeg"
+};
 
-var message = 'Console...';
-
-async.parallel([
-    function (callback) {fs.readFile(__dirname + '/console.html',function (err, data){html=data;})},
-    function (callback) {fs.readFile(__dirname + '/console.css',function (err, data){css=data;})}
-], function(callback) {
-    // create server
-    http.createServer(function(req, res) {
-        var uri = url.parse(req.url).pathname;
-        
-        switch(uri) {
-            case '/cors':
-                if (req.method.toUpperCase() === "OPTIONS"){
-                     res.writeHead(
-                        "204",
-                        "No Content",
-                        {
-                            "Access-Control-Allow-Origin": "*",
-                            "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-                            "Access-Control-Allow-Headers": "Content-Type, Accept",
-                            "Access-Control-Max-Age": 10, // seconds
-                            "Content-Length": 0
-                        }
-                    );
-                    res.end();
-                }
-                else if(req.method.toUpperCase() === "POST"){
-                    var body='';
-                    req.on('data', function (data) {
-                        body += data;
-                    });
-                    req.on('end',function(){
-                        var POST =  qs.parse(body);
-                        message = POST.data;
-                        console.log("guid ="+POST.guid);
-            
-                        res.writeHead(
-                            "200",
-                            "OK",
-                            {
-                                "Access-Control-Allow-Origin": "*",
-                                "Content-Type": "text/plain",
-                                "Content-Length": 1
-                            }
-                        );
-                        res.end('1');
-                    });
-                }
-                else {
-                    res.end("0");
-                }
-                break;
-            case '/stream':
-                res.writeHead(200, {
-                    'Content-Type': 'text/event-stream',
-                    'Cache-Control': 'no-cache',
-                    'Connection': 'keep-alive'
-                });
-                    
-                setInterval(function(){
-                    if (message) {
-                        var id = (new Date()).toLocaleTimeString();
-                                    
-                        res.write('id: ' + id + '\n');
-                        res.write("data: " + message + '\n\n');
-                        
-                        message = '';
-                    }
-                }, 1000);
-                break;
-            case '/console.html':
-                res.end(html);
-                break;
-            case '/console.css':
-                res.end(css);
-                break;
-            default:
-                res.end("Hello world from Cloud9! " + uri);
-        }
-    }).listen(process.env.PORT, process.env.IP);
+// serving static content
+var app = http.createServer(function (req, res) {
+    var uri = url.parse(req.url).pathname;
+    if (uri === '/') uri = '/console.html';
+    var fn = __dirname + uri;
+    var ext = path.extname(fn);
     
-    console.log('Remote Console started');
+    fs.readFile(__dirname + uri, 'utf-8', function (error, data) {
+        res.writeHead(200, {'Content-Type': extensions[ext]});
+        res.write(data);
+        res.end();
+    });
+}).listen(process.env.PORT, process.env.IP);
+
+var io = sck.listen(app);
+ 
+io.sockets.on('connection', function(socket) {
+    socket.on('message_to_server', function(data) {console.log('message_to_server'); console.log(data);
+        io.sockets.emit("message_to_client", {guid: data.guid, message: data.message });
+    });
+    
+    socket.on('command_to_server', function(data) {
+        io.sockets.emit("command_to_client", {guid: data.guid, message: data.message });
+    });
 });
